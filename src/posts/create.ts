6ce766e0832f76f64c1f39087ef955f1a73b4f68
takagi <iam.takagi.dev@gmail.com>
2021-07-24 03:39:53 +0900

    cleanup: log

--- src/posts/create.ts
+++ src/posts/create.ts
@@ -2,7 +2,6 @@ import getIp from "../utils/getIp"
 import { postsRepo } from "../constants"
 
 export default async (ctx) => {
-    console.log(ctx.request.body)
     const ip = await getIp()
     const post = await postsRepo.create({
         address: ip,
