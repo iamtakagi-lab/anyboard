6a35edcdde182a1567d4863fdc8049ed4aba0c96
takagi <iam.takagi.dev@gmail.com>
2021-07-26 20:57:51 +0900

    cleanup: env

--- src/common/env.ts
+++ src/common/env.ts
@@ -3,7 +3,5 @@ export default {
     HOST: process.env.HOST ? process.env.HOST : '0.0.0.0',
     PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
     BASE_URL: process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:3000/api/',
-    USER: process.env.USER ? process.env.USER : 'user',
-    PASS: process.env.PASS ? process.env.PASS : 'pass',
     POST_TEXT_MAX_LENGTH: process.env.POST_TEXT_MAX_LENGTH ? Number(process.env.POST_TEXT_MAX_LENGTH) : 500
 }
