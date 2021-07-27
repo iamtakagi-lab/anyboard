22503f4b5f19a7e90c83b57cd3d93c2743b9c6ec
takagi <iam.takagi.dev@gmail.com>
2021-07-28 03:58:50 +0900

    fix: render [object Object]

--- src/components/posts/post-item.tsx
+++ src/components/posts/post-item.tsx
@@ -4,26 +4,25 @@ import Link from 'next/link'
 
 export const PostItem: React.VFC<{ post: Post }> = ({ post }) => (
   <>
-   
-      <Link href={`/posts/${post.id}`}>
-        <p className="hover:text-gray-300 cursor-pointer">
-          Post ID: {post.id}
-        </p>
-      </Link>
-      <p>書き込んだ人: {post.authorId}さん</p>
-      <p>
-        {post.replyTo.length > 0 ? (
-            post.replyTo.split(',').map((id) => {
-                return (
-                    <Link href={`/posts/${id}`}>
-                        <p className="hover:text-gray-300 cursor-pointer mr-2 inline">
-                        {'>>' + id}
-                        </p>
-                    </Link>
-                )
-            })
-        ) + ' への返信': null}</p>
-      <p>{post.text}</p>
-      <p>{post.postedAt}</p>
+
+    <Link href={`/posts/${post.id}`}>
+      <p className="hover:text-gray-300 cursor-pointer">
+        Post ID: {post.id}
+      </p>
+    </Link>
+    <p>書き込んだ人: {post.authorId}さん</p>
+    <p>
+      {post.replyTo.length > 0 ? 
+        post.replyTo.split(',').map((id) => {
+          return (
+            <Link href={`/posts/${id}`}>
+              <p className="hover:text-gray-300 cursor-pointer mr-2 inline">
+              {'>>'}{id}
+              </p>
+            </Link>
+          )
+        }): null}</p>
+    <p>{post.text}</p>
+    <p>{post.postedAt}</p>
   </>
 )
