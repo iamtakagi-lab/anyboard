766424dfee48fca454d759ad11f48baacf092e39
takagi <iam.takagi.dev@gmail.com>
2021-07-28 00:11:24 +0900

    fix: docker env

--- src/common/env.ts
+++ src/common/env.ts
@@ -2,6 +2,6 @@ export default {
     NODE_ENV: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
     HOST: process.env.HOST ? process.env.HOST : '0.0.0.0',
     PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
-    API_BASE_URL: process.env.API_BASE_URL ? process.env.API_BASE_URL : 'https://anyboard.iamtakagi.net/api/',
+    API_BASE_URL: process.env.API_BASE_URL ? process.env.API_BASE_URL : 'http://localhost:3000/api/',
     POST_TEXT_MAX_LENGTH: process.env.POST_TEXT_MAX_LENGTH ? Number(process.env.POST_TEXT_MAX_LENGTH) : 500
 }
