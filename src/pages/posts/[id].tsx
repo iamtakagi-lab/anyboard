0c491d47ca4251f4ab707ee4a34fdfa28f91342d
takagi <iam.takagi.dev@gmail.com>
2021-07-28 00:24:32 +0900

    fix: id param

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,31 +1,19 @@
 import { Post } from '@prisma/client'
+import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { PostItem } from '../../components/posts/post-item'
 import { useBackend } from '../../hooks/backend'
-import { usePostsRepo } from '../../hooks/repositories'
 
-export const getStaticProps = async ({ params: { id } }) => {
-  return {
-    props: {
-      id
-    }
-  }
-}
-
-export const getStaticPaths = async () => {
-  const posts = await usePostsRepo().getMany()
-  const paths = posts.map(post => `/posts/${post.id.toString()}`)
-  return { paths, fallback: false }
-}
-
-const PostsIdPage: React.VFC<{ id: string }> = ({ id }) => {
+const PostsIdPage: React.VFC<{}> = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
   const [post, setPost] = useState<Post | null>()
   const backend = useBackend()
 
   useEffect(() => {
-    backend.getPost(id).then((data) => {
+    backend.getPost(id.toString()).then((data) => {
       setPost(data)
     })
   }, [])
