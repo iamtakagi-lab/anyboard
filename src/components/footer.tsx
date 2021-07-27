9bb8c7a8e48a4152c623d0b924939ab726243eb0
takagi <iam.takagi.dev@gmail.com>
2021-07-28 03:58:19 +0900

    fix: repository link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.VFC<{}> = () => (
   <footer className="desc w-full my-6 space-y-1 mt-7">
     <a
       className="text-sm underline"
-      href="https://github.com/iamtakagi/anonytify"
+      href="https://github.com/iamtakagi/anyboard"
     >
       View code of this app
       <FiGithub className="inline block ml-1" />
