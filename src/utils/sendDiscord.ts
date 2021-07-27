28b4f3aa4f6028e33210e19e4a034e9976f6717a
takagi <iam.takagi.dev@gmail.com>
2021-07-28 02:43:30 +0900

    fix: support embed

--- src/utils/sendDiscord.ts
+++ src/utils/sendDiscord.ts
@@ -1,22 +1,23 @@
 import { Post } from "@prisma/client";
-import { WebhookClient } from "discord.js";
+import { MessageEmbed, WebhookClient } from "discord.js";
+import { resolve } from "path/posix";
 import env from "../common/env";
 
 export default async (post: Post) => {
-    const splitUrl = env.DISCORD_WEBHOOK_URL.split('/')
-    const webhook = new WebhookClient(splitUrl[5], splitUrl[6])
-    let message
-    message+="-------------------------------\n"
-    message+=post.authorId + "さんが書き込みました\n"
-    const replies = post.replyTo.split(",")
-    if(replies.length > 0) {
-        replies.map((id) => {
-            message += "[>>" + id +  "](" + env.BASE_URL + "/posts/" + id + ") "
-        })
-        message+="への返信\n"
+    if (env.DISCORD_WEBHOOK_URL.length > 0) {
+        const splitUrl = env.DISCORD_WEBHOOK_URL.split('/')
+        const webhook = new WebhookClient(splitUrl[5], splitUrl[6])
+        const embed = new MessageEmbed()
+        embed.setTitle("[" + post.authorId + "さんが書き込みました](" + env.BASE_URL + "/posts/" + post.id)
+        const replies = post.replyTo.split(",")
+        if (replies.length > 0) {
+            let rep = ""
+            replies.map((id) => {
+                rep += "[>>" + id + "](" + env.BASE_URL + "/posts/" + id + ")"
+            })
+            embed.addField("返信先", rep, false)
+        }
+        embed.addField("内容", post.text, false)
+        return await webhook.send(embed)
     }
-    message+=post.text + "\n"
-    message+=post.postedAt
-    message+="-------------------------------"
-    return await webhook.send(message)
 }
