1ede952639a59835edc0a0ee05c27263b13b4891
takagi <iam.takagi.dev@gmail.com>
2021-08-06 17:20:43 +0900

    Update env.ts

--- src/common/env.ts
+++ src/common/env.ts
@@ -2,7 +2,7 @@ export default {
     NODE_ENV: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
     HOST: process.env.HOST ? process.env.HOST : '0.0.0.0',
     PORT: process.env.PORT ? Number(process.env.PORT) : 3010,
-    BASE_URL: process.env.BASE_URL ? process.env.BASE_URL : 'https://anyboard.iamtakagi.net/',
+    BASE_URL: process.env.BASE_URL ? process.env.BASE_URL : 'https://anyboard.iamtakagi.net',
     API_BASE_URL: process.env.API_BASE_URL ? process.env.API_BASE_URL : 'https://anyboard.iamtakagi.net/api',
     POST_TEXT_MAX_LENGTH: process.env.POST_TEXT_MAX_LENGTH ? Number(process.env.POST_TEXT_MAX_LENGTH) : 500,
     DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL ? process.env.DISCORD_WEBHOOK_URL : ''
