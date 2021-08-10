156abdc3ee2807a04808cc059e7e451ed5737652
takagi <iam.takagi.dev@gmail.com>
2021-08-10 21:28:19 +0900

    resolve: query 2

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -4,18 +4,19 @@ import { useState } from 'react'
 import { PostItem } from '../../components/posts/post-item'
 import { useBackend } from '../../hooks/backend'
 
-export const getInitialProps = ({query}) => {
+export const getInitialProps = async ({query}) => {
+  const { id } = query
   return {
-    query
+    id
   }
 }
 
-const PostsIdPage: React.VFC<{query}> = ({query}) => {
+const PostsIdPage: React.VFC<{id}> = ({id}) => {
   const [post, setPost] = useState<Post | null>()
   const backend = useBackend()
 
   useEffect(() => {
-    backend.getPost(query.id).then((data) => {
+    backend.getPost(id).then((data) => {
       setPost(data)
     })
   }, [])
