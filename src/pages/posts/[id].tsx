2049f3fd223534b80a2f7d78b30f751f92ccfa06
takagi <iam.takagi.dev@gmail.com>
2021-08-10 20:57:28 +0900

    resolve: params query

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,19 +1,21 @@
 import { Post } from '@prisma/client'
-import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { PostItem } from '../../components/posts/post-item'
 import { useBackend } from '../../hooks/backend'
 
-const PostsIdPage: React.VFC<{}> = () => {
-  const router = useRouter();
-  const { id } = router.query;
+export const getInitialProps = ({query}) => {
+  return {
+    query
+  }
+}
 
+const PostsIdPage: React.VFC<{query}> = ({query}) => {
   const [post, setPost] = useState<Post | null>()
   const backend = useBackend()
 
   useEffect(() => {
-    backend.getPost(id.toString()).then((data) => {
+    backend.getPost(query.id).then((data) => {
       setPost(data)
     })
   }, [])
